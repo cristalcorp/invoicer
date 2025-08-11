c9c53650e45f98b9c606774b71f9f45146c4712e
cristalcorp <cristal@cristalcorp.com>
2025-08-11 10:46:41 +0200

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  prefetch: true,
+  trailingSlash: 'always',
+  output: 'static',
+  site: 'https://tonpseudo.github.io/invoicer/',
+  base: '/invoicer/'
+});
